d65397d2e26f00107e0d080efb2a40c2c7b7a6d3
XAjviX <42722807+XAjviX@users.noreply.github.com>
2018-08-28 01:05:41 +0200

    Zegar kappa
    Jelon to kozak

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 const zegarElement = document.querySelector("#zegar");
+const sarnaElement = document.querySelector(".obrazekSarna");
 let czasNaStronie = 0;
 
 setInterval(() => {
-  zegarElement.innerHTML = czasNaStronie;
+  if (czasNaStronie % 2 === 0) {
+    zegarElement.innerHTML = czasNaStronie;
+    sarnaElement.style.display = "block";
+  } else {
+    zegarElement.innerHTML = "kappa";
+    sarnaElement.style.display = "none";
+  }
   czasNaStronie++;
 }, 1000);
