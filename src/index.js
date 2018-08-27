06e6a03ec71c0f5ae302e7c14a24871ebcb0bb86
XAjviX <42722807+XAjviX@users.noreply.github.com>
2018-08-28 01:18:05 +0200

    Jelenisko
    Kozacko

--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ const sarnaElement = document.querySelector(".obrazekSarna");
 let czasNaStronie = 0;
 
 setInterval(() => {
+  // gdy czas na stronie podzielony przez 2 daje reszte 0
   if (czasNaStronie % 2 === 0) {
+    //  to ustaw w zegarElemet html o tresci czas na stronie
     zegarElement.innerHTML = czasNaStronie;
-    sarnaElement.style.display = "block";
+
+    // ustaw tez styl display block dla obrazka
+    // sarnaElement.style.display = "block";
   } else {
+    // w innym wypadku
+    // ustaw zegar na tresc kappa
     zegarElement.innerHTML = "kappa";
-    sarnaElement.style.display = "none";
   }
+
+  sarnaElement.classList.toggle("rozmazanaSarna");
   czasNaStronie++;
 }, 1000);
